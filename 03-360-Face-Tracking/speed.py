2f7f7a9896e9259bf179e5f5bc591721a1763547
Florian <113524385+Florianrcht@users.noreply.github.com>
2025-04-10 21:41:18 +0200

    try input

--- 03-360-Face-Tracking/speed.py
+++ 03-360-Face-Tracking/speed.py
@@ -1,23 +1,8 @@
 import requests
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((400,400))
 
 try:
-    done = False
-    while not done:
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    print("pressed w")
-                if event.key == pygame.K_s:
-                    print("pressed s")
-                    
-    response = requests.get('http://localhost:5000/speed')
+    x = input()
+    response = requests.get('http://localhost:5000/speed/'+x)
     print(response.text)
 except Exception as e:
     print(f"Erreur lors de la requête : {e}")
