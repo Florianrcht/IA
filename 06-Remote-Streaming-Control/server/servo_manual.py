70cfe1592e9fcd36bf23f7b5543c9105d40f0505
Florian <113524385+Florianrcht@users.noreply.github.com>
2025-05-24 18:48:44 +0200

    servo manual control

--- 06-Remote-Streaming-Control/server/servo_manual.py
+++ 06-Remote-Streaming-Control/server/servo_manual.py
@@ -17,27 +17,13 @@ def servo_function():
         "servoVitesse": 0
     }
 
-    async def looper():
-        while state["servoControl"]:
-            p.ChangeDutyCycle(state["servoVitesse"])
-            print("Vitesse:", state["servoVitesse"])
-            await asyncio.sleep(2)
-            state["servoVitesse"] = int(46)
-
-
+    @app.route('/kill')
+    def kill():
+        state["servoControl"] = False
+        print("Commande reçue : FIN")
         p.stop()                 
         GPIO.cleanup()
         print("Servo arrêté et GPIO nettoyé")
-
-    def start_async_loop():
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(looper())
-
-    @app.route('/stop')
-    def stop():
-        state["servoControl"] = False
-        print("Commande reçue : STOP")
         return "Servo arrêté"
 
     @app.route('/speed/<servoVitesseRequest>')
@@ -45,13 +31,10 @@ def servo_function():
         try:
             state["servoVitesse"] = int(servoVitesseRequest)
             print("Commande reçue : vitesse =", state["servoVitesse"])
+            p.ChangeDutyCycle(state["servoVitesse"])
             return f"Vitesse mise à jour : {state['servoVitesse']}"
         except ValueError:
             return "Erreur : la vitesse doit être un entier."
-
-    # Lancer la boucle asynchrone dans un thread
-    thread = threading.Thread(target=start_async_loop)
-    thread.start()
 
     # Lancer le serveur Flask
     app.run(host="0.0.0.0", port=5000)
