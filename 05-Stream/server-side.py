fb056bb5bd392d85d6c892d7b1eb7584c3003366
Florian <113524385+Florianrcht@users.noreply.github.com>
2025-05-24 13:46:58 +0200

    reduce image height width

--- 05-Stream/server-side.py
+++ 05-Stream/server-side.py
@@ -21,6 +21,8 @@ while True:
     print('GOT CONNECTION FROM:',addr)
     if client_socket:
         vid = cv2.VideoCapture(0) #1 pour Mac
+        vid.set(cv2.CAP_PROP_FRAME_WIDTH, 320) #Réduis la taille de l'image
+        vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
         
         while(vid.isOpened()):
             print('Stream')
