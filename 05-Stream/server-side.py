762bdb584fe15737cf54a1dc75f21f1458a396c1
Florian <113524385+Florianrcht@users.noreply.github.com>
2025-05-24 13:06:33 +0200

    fix

--- 05-Stream/server-side.py
+++ 05-Stream/server-side.py
@@ -3,11 +3,11 @@ import socket, cv2, pickle,struct
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
+#host_ip = socket.gethostbyname(host_name)
+host_ip = ''
 print('HOST IP:',host_ip)
 port = 9999
-#socket_address = (host_ip,port)
-socket_address = ('',port)
+socket_address = (host_ip,port)
 
 # Socket Bind
 server_socket.bind(socket_address)
@@ -21,7 +21,7 @@ while True:
     client_socket,addr = server_socket.accept()
     print('GOT CONNECTION FROM:',addr)
     if client_socket:
-        vid = cv2.VideoCapture(1)
+        vid = cv2.VideoCapture(0) #1 pour Mac
         
         while(vid.isOpened()):
             print('Stream')
